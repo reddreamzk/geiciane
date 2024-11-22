7a7dbf0c6a051b47f0f4ae90c3f8928f0324fea6
Bruno <Brunoverstappen@zohomail.com>
2024-11-22 15:09:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('contadores-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/momentos.mp4',
+                '/music.mp3',
+                '/icons/icon-192x192.png',
+                '/icons/icon-512x512.png'
+            ]);
+        })
+    );
+});
 
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
