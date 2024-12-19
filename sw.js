c88b722feb5fec77e41fe94f786ac20df4d58350
Bruno <Brunoverstappen@zohomail.com>
2024-12-19 18:45:36 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -10,10 +10,8 @@ self.addEventListener('install', function(event) {
                 '/natal.mp4',
                 '/ano_novo.mp4',
                 '/pascoa.mp4',
-                '/dia_dos_namorados.mp4',
-                '/aniversário_amor.mp4',
-                '/aniversário_bruno.mp4',
                 '/music.mp3',
+                '/text.json',
                 '/icons/icon-192x192.png',
                 '/icons/icon-512x512.png'
             ]);
